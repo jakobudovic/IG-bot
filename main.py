e09aa64e8f8543c356c0f97a98126c3e55de72e2
Jakob Udovic <ju8168@student.uni-lj.si>
2020-05-02 22:13:35 +0200

    now like_posts() likes only unliked user posts

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as expect
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys                         # for escape and such
+
 
 
 class InstaBot:
@@ -13,7 +15,7 @@ class InstaBot:
         self.username = username
         self.driver.get("https://instagram.com")
 
-        wait = WebDriverWait(self.driver, 10)
+        wait = WebDriverWait(self.driver, 20)
 
         el = wait.until(EC.visibility_of_element_located(
             (By.NAME, 'username')))
@@ -42,26 +44,79 @@ class InstaBot:
             (By.XPATH, "//a[contains(@href,'/{}/')]".format(user))))
         el.click()
 
-    def like_posts(self):
+    def like_posts(self, user):
         # first post
         wait = WebDriverWait(self.driver, 10)
+
+        # wait until the profile is actually loaded
+        print("waiting on matching name", user)
+        el = wait.until(EC.presence_of_element_located(
+            (By.XPATH, "//h2[@class='_7UhW9       fKFbl yUEEX   KV-D4            fDxYl     ']")))
+        print(el.text)
+        
+        while el.text != user:
+            sleep(2)
+            el = wait.until(EC.presence_of_element_located(
+            (By.XPATH, "//h2[@class='_7UhW9       fKFbl yUEEX   KV-D4            fDxYl     ']")))
+            print(el.text)
+
+        # get number of posts
         el = wait.until(EC.visibility_of_element_located(
-            (By.XPATH, "/html/body/div[1]/section/main/div/div[2]/article/div/div/div[1]/div[1]")))
+            (By.CLASS_NAME, 'g47SY ')))
+        num_posts = int(el.text)
+        print("\nnumber of posts:", num_posts)
+
+
+
+        el = wait.until(EC.visibility_of_element_located(
+            # (By.XPATH, "/html/body/div[1]/section/main/div/div[2]/article/div/div/div[1]/div[1]")))
+            (By.CLASS_NAME, "eLAPa")))
+            # try: classes eLAPa, KL4Bh, FFVAD
         el.click()
 
-        while True:
-            print("new post liked")
-            # like
-            el = wait.until(EC.visibility_of_element_located(
+        new_likes = 0
+
+        for i in range(num_posts):
+            
+            # wait for the visible heart first
+            heart = wait.until(EC.visibility_of_element_located(
                 (By.XPATH, "//span[@class='fr66n']")))
-            el.click()
+            
+            # get the color of the like heart
+            color = self.driver.find_element_by_css_selector('section>span>button>svg').get_attribute('fill')
+
+            if color == "#262626":
+                # unlike: fill="#262626"
+                # already liked: fill="#ed4956"
+                heart.click()
+                ++new_likes
+                # print("post", i+1, "liked")
 
             # next
-            self.driver.find_element_by_link_text('Next')\
-                .click()
+            if i+1 < num_posts:
+                self.driver.find_element_by_link_text('Next')\
+                    .click()
+            else:
+                # close (last) post
+                webdriver.ActionChains(self.driver).send_keys(Keys.ESCAPE).perform()
+        
+        print("\nNumber of new likes for the user", user, new_likes, "!\n")
 
 
 myBot = InstaBot("jakobudovic", pw)
 # myBot.search_user("wrongcountryfool")
-myBot.search_user("wrongcountryfool")
-myBot.like_posts()
+
+users = ["aljadostal", "og_loc420_69", "wrongcountryfool"]
+
+myBot.search_user("ogloc42069")
+myBot.like_posts("ogloc42069")
+
+
+"""
+for user in users:
+    myBot.search_user(user)
+    print("\n---------------------------------)
+    print(liking users post from: ", user))
+    myBot.like_posts(user)
+"""
+print("all users covered!")
---
+++ useful-links.md
@@ -0,0 +1,17 @@
+wait: 
+https://stackoverflow.com/questions/46213397/why-cant-i-find-an-input-element-with-a-placeholder-in-selenium
+
+waits Doc: 
+https://selenium-python.readthedocs.io/waits.html
+
+wait example: 
+https://www.edureka.co/community/6983/how-to-get-text-found-between-span-selenium
+
+a lot of waiting examples: 
+https://www.programcreek.com/python/example/88684/selenium.webdriver.support.expected_conditions.visibility_of_element_located
+
+locating elements docs: https://selenium-python.readthedocs.io/locating-elements.html
+By.____ options: 
+
+Last answer, got me the like element: 
+https://stackoverflow.com/questions/14049983/selenium-webdriver-finding-an-element-in-a-sub-element
